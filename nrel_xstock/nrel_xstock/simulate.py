e28317bb4aeb7f0c06b6b404dfb954521707b716
Kingsley Nweye <etonwana@yahoo.com>
2021-12-13 18:20:28 -0600

    refactoring & added support for ideal load system

--- nrel_xstock/nrel_xstock/simulate.py
+++ nrel_xstock/nrel_xstock/simulate.py
@@ -2,6 +2,86 @@ from io import StringIO
 import os
 from eppy.modeleditor import IDF
 from openstudio import energyplus, osversion
+from nrel_xstock.database import SQLiteDatabase
+from nrel_xstock.utilities import write_data, get_data_from_path
+
+
+class OpenStudioModelEditor:
+    def __init__(self,osm):
+        self.osm = osm
+
+    @property
+    def osm(self):
+        return self.__osm
+
+    @osm.setter
+    def osm(self,osm):
+        self.__osm = get_data_from_path(osm)
+
+    def forward_translate(self):
+        osm = self.__get_model()
+        forward_translator = energyplus.ForwardTranslator()
+        idf = forward_translator.translateModel(osm)
+        idf = str(idf)
+        return idf
+
+    def use_ideal_loads_air_system(self):
+        # Reference: https://www.rubydoc.info/gems/openstudio-standards/Standard#remove_hvac-instance_method
+        osm = self.__get_model()
+
+        # remove air loop hvac
+        for air_loop in osm.getAirLoopHVACs():
+            air_loop.remove()
+
+        # remove plant loops
+        for plant_loop in osm.getPlantLoops():
+            shw_use = False
+
+            for component in plant_loop.demandComponents():
+                if component.to_WaterUseConnections().is_initialized() or component.to_CoilWaterHeatingDesuperheater().is_initialized():
+                    shw_use = True
+                    break
+                else:
+                    pass
+
+            if  not shw_use:
+                plant_loop.remove()
+            else:
+                continue
+
+        # remove vrf
+        for ac_refrigerant_flow in osm.getAirConditionerVariableRefrigerantFlows():
+            ac_refrigerant_flow.remove()
+
+        for terminal_unit_refrigerant_flow in osm.getZoneHVACTerminalUnitVariableRefrigerantFlows():
+            terminal_unit_refrigerant_flow.remove()
+
+        # remove zone equipment
+        for zone in osm.getThermalZones():
+            for equipment in zone.equipment():
+                if not equipment.to_FanZoneExhaust().is_initialized():
+                    equipment.remove()
+                else:
+                    pass
+
+        # remove unused curves
+        for curve in osm.getCurves():
+            if curve.directUseCount() == 0:
+                curve.remove()
+            else:
+                pass
+
+        # add ideal load system
+        for zone in osm.getThermalZones():
+            zone.setUseIdealAirLoads(True)
+            
+        osm = str(osm)
+        self.osm = osm
+
+    def __get_model(self):
+        version_translator = osversion.VersionTranslator()
+        osm = version_translator.loadModelFromString(self.osm).get()
+        return osm
 
 class Simulator:
     def __init__(self,idd_filepath,idf,epw,simulation_id=None,output_directory=None):
@@ -38,11 +118,11 @@ class Simulator:
 
     @idf.setter
     def idf(self,idf):
-        self.__idf = Simulator.get_data_from_path(idf)
+        self.__idf = get_data_from_path(idf)
 
     @epw.setter
     def epw(self,epw):
-        epw = Simulator.get_data_from_path(epw)
+        epw = get_data_from_path(epw)
         self.__epw = epw
 
     @simulation_id.setter
@@ -71,7 +151,7 @@ class Simulator:
             'output_prefix':self.simulation_id,
             'output_suffix':'C',
             'output_directory':self.output_directory,
-            'readvars':False,
+            'readvars':True,
             'expandobjects':True,
             'verbose':'q',
         }
@@ -80,42 +160,30 @@ class Simulator:
 
     def __write_epw(self):
         filepath = os.path.join(self.output_directory,'weather.epw')
-        
-        with open(filepath,'w') as f:
-            f.write(self.epw)
-
+        write_data(self.epw,filepath)
         self.__epw_filepath = filepath
 
     def preprocess(self):
         raise NotImplementedError
-    
-    @staticmethod
-    def osm_to_idf(osm):
-        osm = Simulator.get_data_from_path(osm)
-        version_translator = osversion.VersionTranslator()
-        osm = version_translator.loadModelFromString(osm).get()
-        forward_translator = energyplus.ForwardTranslator()
-        idf = forward_translator.translateModel(osm)
-        idf = str(idf)
-        return idf
-
-    @staticmethod
-    def get_data_from_path(filepath):
-        if os.path.exists(filepath):
-            with open(filepath,mode='r') as f:
-                data = f.read()
-        else:
-            data = filepath
-
-        return data
 
 class CityLearnSimulator(Simulator):
-    def __init__(self,idd_filepath,idf,epw,ddy=None,**kwargs):
+    def __init__(self,idd_filepath,idf,epw,**kwargs):
         super().__init__(idd_filepath,idf,epw,**kwargs)
 
     def simulate(self,**run_kwargs):
         self.preprocess()
         super().simulate(**run_kwargs)
+
+    def get_simulation_result(self,query_filepath):
+        with open(query_filepath,'r') as f:
+            query = f.read()
+
+        database = SQLiteDatabase(os.path.join(self.output_directory,f'{self.simulation_id}.sql'))
+        data = database.query_table(query)
+        # Parantheses in column names changed to square braces to match CityLearn format
+        # SQLite3 ignores square braces in column names so parentheses used as temporary fix. 
+        data.columns = [c.replace('(','[').replace(')',']') for c in data.columns]
+        return data
 
     def preprocess(self):
         idf = IDF(StringIO(self.idf))
@@ -123,34 +191,32 @@ class CityLearnSimulator(Simulator):
         obj = idf.idfobjects['Timestep'][0]
         obj.Number_of_Timesteps_per_Hour = 1
 
+        # *********** remove energy management system objects ***********
+        # Cannot gurantee that removing these objects will retain the intended building systems
+        # operation and behavior. However, the objects need to be removed in order to avoid simulation
+        # errors when using IdealLoads object.
+        ems_objects = [   
+            'EnergyManagementSystem:Sensor',
+            'EnergyManagementSystem:Actuator',
+            'EnergyManagementSystem:Subroutine',
+            'EnergyManagementSystem:Program',
+            'EnergyManagementSystem:ProgramCallingManager',
+            'EnergyManagementSystem:OutputVariable',
+        ]
+
+        for ems_object in ems_objects:
+            idf.idfobjects[ems_object] = []
+
         # *********** update output variables ***********
         output_variables = {
-            'Equipment Electric Power': [
-                'Lights Electricity Energy',
-                'Electric Equipment Electricity Energy',
-            ],
-            'Indoor Temperature [C]':[
-                'Zone Air Temperature',
-            ],
-            'Indoor Relative Humidity [%]':[
-                'Zone Air Relative Humidity'
-            ],
-            'Average Unmet Cooling Setpoint Difference [C]':[
-                'Zone Thermostat Cooling Setpoint Temperature'
-            ],
-            'Average Unmet Heating Setpoint Difference [C]':[
-                'Zone Thermostat Heating Setpoint Temperature'
-            ],
-            'DHW Heating [kWh]':[
-                'Water Heater Heating Energy'
-            ],
-            'Cooling Load [kWh]':[
-                'Zone Predicted Sensible Load to Cooling Setpoint Heat Transfer Rate',
-                'Zone Predicted Moisture Load Moisture Transfer Rate',
-            ],
-            'Heating Load [kWh]':[
-                'Zone Predicted Sensible Load to Heating Setpoint Heat Transfer Rate',
-            ],
+            'Equipment Electric Power': ['Lights Electricity Energy','Electric Equipment Electricity Energy',],
+            'Indoor Temperature [C]':['Zone Air Temperature',],
+            'Indoor Relative Humidity [%]':['Zone Air Relative Humidity'],
+            'Average Unmet Cooling Setpoint Difference [C]':['Zone Thermostat Cooling Setpoint Temperature'],
+            'Average Unmet Heating Setpoint Difference [C]':['Zone Thermostat Heating Setpoint Temperature'],
+            'DHW Heating [kWh]':['Water Heater Heating Energy'],
+            'Cooling Load [kWh]':['Zone Ideal Loads Zone Total Cooling Energy',],
+            'Heating Load [kWh]':['Zone Ideal Loads Zone Total Heating Energy',],
         }
         idf.idfobjects['Output:Variable'] = []
 
